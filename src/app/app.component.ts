22b114e53dce14e9eac7b806187f691a1a098ade
Kiryl Bahdanau <kirbah@gmail.com>
2025-02-25 07:35:20 +0100

    fix RouterOutlet warning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { AddItemComponent } from './components/add-item/add-item.component';
 import { ItemsComponent } from './components/items/items.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, HeaderComponent, ItemsComponent, AddItemComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [HeaderComponent, ItemsComponent, AddItemComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {}
